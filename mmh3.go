d8592c888cda0caadbbf738071f2e70ed4d924fd
Michael Roberson <michaelroberson@gmail.com>
2019-07-09 20:52:04 -0400

    Add doc comments in mmh3.go, format code, and rename exported functions.

--- mmh3.go
+++ mmh3.go
@@ -2,8 +2,9 @@ package mmh3
 
 import (
 	"encoding/binary"
-	"github.com/roberson-io/mmh3/MurmurHash3"
 	"unsafe"
+
+	"github.com/roberson-io/mmh3/MurmurHash3"
 )
 
 // Endianness checking pattern from TensorFlow
@@ -27,27 +28,27 @@ func init() {
 type mmh3Function func(uintptr, int, MurmurHash3.Uint32_t, uintptr)
 
 func mmh3Hash(key []byte, seed uint32, is32Bit bool, mmh3Func mmh3Function) []byte {
-    var data32 []uint32
-    var data64 []uint64
-    var blockSize, nBytes int
-    var ptrKey uintptr
-    if is32Bit {
-        blockSize = 4
-        nBytes = 4
-        for i := 0; i < len(key); i += blockSize {
-            block := nativeEndian.Uint32(key[i : i+blockSize])
-            data32 = append(data32, block)
-        }
-        ptrKey = uintptr(unsafe.Pointer(&data32[0]))
-    } else {
-        blockSize = 8
-        nBytes = 16
-        for i := 0; i < len(key); i += blockSize {
-            block := nativeEndian.Uint64(key[i : i+blockSize])
-            data64 = append(data64, block)
-        }
-        ptrKey = uintptr(unsafe.Pointer(&data64[0]))
-    }
+	var data32 []uint32
+	var data64 []uint64
+	var blockSize, nBytes int
+	var ptrKey uintptr
+	if is32Bit {
+		blockSize = 4
+		nBytes = 4
+		for i := 0; i < len(key); i += blockSize {
+			block := nativeEndian.Uint32(key[i : i+blockSize])
+			data32 = append(data32, block)
+		}
+		ptrKey = uintptr(unsafe.Pointer(&data32[0]))
+	} else {
+		blockSize = 8
+		nBytes = 16
+		for i := 0; i < len(key); i += blockSize {
+			block := nativeEndian.Uint64(key[i : i+blockSize])
+			data64 = append(data64, block)
+		}
+		ptrKey = uintptr(unsafe.Pointer(&data64[0]))
+	}
 	ptrSeed := MurmurHash3.SwigcptrUint32_t(unsafe.Pointer(&seed))
 	hash := make([]byte, nBytes)
 	ptrHash := uintptr(unsafe.Pointer(&hash[0]))
@@ -55,17 +56,26 @@ func mmh3Hash(key []byte, seed uint32, is32Bit bool, mmh3Func mmh3Function) []by
 	return hash
 }
 
-func Hash_x86_32(key []byte, seed uint32) []byte {
-    is32Bit := true
-    return mmh3Hash(key, seed, is32Bit, MurmurHash3.MurmurHash3_x86_32)
+// Hashx86_32 is a helper function for MurmurHash3_x86_32 in the original C++
+// implementation. It accepts data to be hashed as a byte slice as well as a
+// seed value. It returns a byte slice with the hash.
+func Hashx86_32(key []byte, seed uint32) []byte {
+	is32Bit := true
+	return mmh3Hash(key, seed, is32Bit, MurmurHash3.MurmurHash3_x86_32)
 }
 
-func Hash_x86_128(key []byte, seed uint32) []byte {
-    is32Bit := false
-    return mmh3Hash(key, seed, is32Bit, MurmurHash3.MurmurHash3_x86_128)
+// Hashx86_128 is a helper function for MurmurHash3_x86_128 in the original C++
+// implementation. It accepts data to be hashed as a byte slice as well as a
+// seed value. It returns a byte slice with the hash.
+func Hashx86_128(key []byte, seed uint32) []byte {
+	is32Bit := false
+	return mmh3Hash(key, seed, is32Bit, MurmurHash3.MurmurHash3_x86_128)
 }
 
-func Hash_x64_128(key []byte, seed uint32) []byte {
-    is32Bit := false
-    return mmh3Hash(key, seed, is32Bit, MurmurHash3.MurmurHash3_x64_128)
+// Hashx64_128 is a helper function for MurmurHash3_x64_128 in the original C++
+// implementation. It accepts data to be hashed as a byte slice as well as a
+// seed value. It returns a byte slice with the hash.
+func Hashx64_128(key []byte, seed uint32) []byte {
+	is32Bit := false
+	return mmh3Hash(key, seed, is32Bit, MurmurHash3.MurmurHash3_x64_128)
 }
